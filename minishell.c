d538520d6e466da54e85d3c0ad6de6e7d34b86dc
Kritartha Adhikari <kritarthadybala10@gmail.com>
2026-04-04 20:15:32 +0545

    structure2.0

--- minishell.c
+++ minishell.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
 #include "minishell.h"
 
 int main(){
-
+    printf("Hello");
+    return EXIT_SUCCESS;
 }
+
+/*When you use return EXIT_SUCCESS;
+ at the end of your main function, 
+ it translates to returning the integer 0.
+*/
--- minishell.h
+++ minishell.h
@@ -1,7 +1,8 @@
+#ifndef MINISHELL_H //if not defined minishell.h
+#define MINISHELL_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
 
-    
-}
+#endif
